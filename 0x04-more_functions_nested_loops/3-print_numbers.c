8b9e2cc7ed9fccc55cc6a161c5e4544311e01feb
Lusiarwmaba <rwambalusia@gmail.com>
2023-02-24 14:57:27 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function that print numbers
+ * Return: the numbers
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0 ; x = 9 ; x++)
+	putchar(x + '0');
+	putchar('\n');
+		return (0);
+}
