6ab765d08cf16459351b6aef646528ce8032396d
Lusiarwmaba <rwambalusia@gmail.com>
2023-02-24 16:28:36 -0800

    Task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
+
 /**
  * print_numbers - print 0-9
- * _putchar: print the number
- * Return: void
+ * return: void
  */
 
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0' ; c <= '9' ; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-		
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,19 @@
 #include "main.h"
+
 /**
- * print_most_numbers - prints numbers from 0 to 9
- * description: prints the numbers excluding 2 and 4
- * Return: the numbers since 0 to 9
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
  */
+
 void print_most_numbers(void)
 {
-	int x = 0;
+	char c;
 
-	for (; x <= 9; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
-			_putchar('\n');
-}
-	
+	_putchar('\n');
+}	
