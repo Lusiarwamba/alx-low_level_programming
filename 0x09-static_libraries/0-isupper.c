d5925ec20fdf229ce35b65601424a92b6e763c9a
Lusiarwmaba <rwambalusia@gmail.com>
2023-03-19 13:22:32 -0700

    static libraries task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - functions that checks uppercase characters
+ * @c: char to be checked
+ *
+ * Return 0 or 1
+ */
+int _islower(int c)
+{
+	if (c = 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * strlen - function that determines the length of a string
+ * @s: length of string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
